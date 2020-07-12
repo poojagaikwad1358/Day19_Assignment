17572e89b9e6c29f082348abbe1a670605674016
poojagaikwad1358 <poojagaikwad1358@gmail.com>
2020-07-12 23:46:57 +0530

    Solving using switch case statement

--- employeeWageComputation.java
+++ employeeWageComputation.java
@@ -1,21 +1,34 @@
 public class employeeWageComputation {
 
      public static void main(String[] args) {
-	System.out.println("Welcome to employee wage computation Problem..");
+	 System.out.println("Welcome to employee wage computation Problem..");
 
-	int IsFullTime=1;
-	int IsPartTime=2;
-	int empRatePerHrs=20;
-	int empHrs=0;
-	int empwage=0;
+	 public static final int is_fullTime=1;
+         public static final int is_PartTime=2;
+         public static final int ratePerHrs=20;
 
-	double empcheck=Math.floor(Math.random() * 10)%3;
-	if ( empcheck == IsFullTime)
-		empHrs=8;
-	else if ( empcheck == IsPartTime )
-		empHrs=4;
+        public static void main(String[] args)
+        {
+                //variables
+                int empHrs=0;
+                int empWage=0;
+                //computation
+                double randomCheck=Math.floor(Math.random()*10)%3;
 
-		empwage=empHrs*empRatePerHrs;
-		System.out.println("Employee Wage= "+empwage);
+                switch ((int)randomCheck)
+                {
+                case is_fullTime:
+                                empHrs=8;
+                                break;
+                case is_PartTime:
+                                empHrs=4;
+                                break;
+                default:
+                        empHrs=0;
+                        break;
+                }
+                empWage=ratePerHrs*empHrs;
+                System.out.println("Employee Wage : "+empWage);
+
 	}
 }
