073625754bc8beb744d9049370d1a0cbf3f20475
poojagaikwad1358 <poojagaikwad1358@gmail.com>
2020-07-13 00:33:48 +0530

    Calculate total wage

--- employeeWageComputation.java
+++ employeeWageComputation.java
@@ -1,40 +1,40 @@
 public class employeeWageComputation {
 
-         //Constants
-         public static final int is_fullTime=1;
-         public static final int is_PartTime=2;
-         public static final int ratePerHrs=20;
-         public static final int numberOfDays=20;
-        public static void main(String[] args)
-        {
-		 System.out.println("Welcome to Employee wage computation problem..");
-                //variables
-                int empHrs=0;
-                int empWage=0;
-                int totalEmpWage=0;
-                //computation
-                for (int day=0; day<numberOfDays; day++)
-                {
-                        double randomCheck=Math.floor(Math.random()*10)%3;
+		public static final int is_fullTime=1;
+        	public static final int is_PartTime=2;
+  	        public static final int ratePerHrs=20;
+		public static final int NumberOfWorkingDays=20;
+		public static final int MaxHrsInMonth=10;
 
-                        switch ((int)randomCheck)
-                        {
-                        case is_fullTime:
-                                        empHrs=8;
-                                        break;
-                        case is_PartTime:
-                                        empHrs=4;
-                                        break;
-                        default:
-                                        empHrs=0;
-                                        break;
-                        }
-                        totalEmpWage+=empWage;
-                        empWage=ratePerHrs*empHrs;
-                        System.out.println("Employee Wage : "+empWage);
-                }
+	public static void main(String[] args) {
 
-                System.out.println("Total employee wage: "+totalEmpWage);
+	System.out.println("Welcome to Employee Wage Computation problem..");
+	//variables
+	int empWage=0;
+	int EmpHrs=0;
+	int TotalWorkingDays=0;
+	//computation
+	while (empWage <= MaxHrsInMonth && TotalWorkingDays < NumberOfWorkingDays) 
+	{
+		TotalWorkingDays++;
+		int empcheck=((int)Math.floor(Math.random() * 10))%3;
+		switch (( empcheck ))
+		{
+			case is_fullTime:
+				EmpHrs=8;
+				break;
+		        case is_PartTime:
+				EmpHrs=4;
+				break;
+		        default:
+				EmpHrs=0;
+		}
 
-        }
+		empWage=ratePerHrs*EmpHrs;
+		System.out.println("Employee wage: " + empWage);
+
+	}
+	     int TotalEmpWage = empWage * ratePerHrs; //UseCase6- Total employee wage
+	     System.out.println("Total Emp Wage: " +TotalEmpWage);
+	}
 }
